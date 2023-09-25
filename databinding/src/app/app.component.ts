71ad95ea0f69dce3901d1a3c2d7d3a466debef9e
Attila Horvath <sweeptic@gmail.com>
2023-09-25 17:12:03 +0200

    databinding starting files

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -3,35 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   serverElements = [];
+  newServerName = '';
+  newServerContent = '';
 
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+  onAddServer() {
     this.serverElements.push({
       type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
+      name: this.newServerName,
+      content: this.newServerContent
     });
   }
 
-  onBlueprintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
+  onAddBlueprint() {
     this.serverElements.push({
       type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
+      name: this.newServerName,
+      content: this.newServerContent
     });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changed';
-  }
-
-  onDestroyFirst() {
-    this.serverElements.splice(0, 1);
   }
 }
