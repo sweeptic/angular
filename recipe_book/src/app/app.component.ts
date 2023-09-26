00e1cbfbed853ccdde7b832f413b61e6d1370618
Attila Horvath <sweeptic@gmail.com>
2023-09-26 15:53:31 +0200

    add simple navigation

--- recipe_book/src/app/app.component.ts
+++ recipe_book/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  selectedPage = 'recipes';
+
+  onSelectedMenu(page: string) {
+    console.log('page', page);
+    this.selectedPage = page;
+  }
+
   title = 'recipe_book';
 }
--- recipe_book/src/app/main-elements/header/header-component.ts
+++ recipe_book/src/app/main-elements/header/header-component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header-component.html',
 })
 export class HeaderComponent {
+  @Output() selectedMenu = new EventEmitter<string>();
+
+  selectMenu(page: string) {
+    this.selectedMenu.emit(page);
+  }
+
   constructor() {}
 }
