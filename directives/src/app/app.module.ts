85ee08a7ca7b70d1f0f0b05908e61aae2976e263
Attila Horvath <sweeptic@gmail.com>
2023-09-27 17:32:33 +0200

    add descriptions

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-hightlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless.directive';
+import { BetterHighlightDirective } from './custom-attribute-directive/better-highlight.directive';
+import { UnlessDirective } from './custom-structural-directive/unless.directive';
 
 @NgModule({
   declarations: [
