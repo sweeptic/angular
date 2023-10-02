1460cff20b7878dd12c02c8b33e5e11d42c60dd5
Attila Horvath <sweeptic@gmail.com>
2023-10-02 14:21:40 +0200

    131 - Navigating Programmatically

--- routing-start-2/src/app/home/home.component.ts
+++ routing-start-2/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLoadServers() {
+    // complex calculation
+    // console.log('router', this.router);
+    this.router.navigate(['/servers']);
   }
-
 }
