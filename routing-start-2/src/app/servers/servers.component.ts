54acede481acbf5e826dc56e0d463828bcad3346
Attila Horvath <sweeptic@gmail.com>
2023-10-02 14:41:16 +0200

    using reload ( doesnt work yet)

--- routing-start-2/src/app/servers/servers.component.ts
+++ routing-start-2/src/app/servers/servers.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // console.log('route', this.route);
+    // this.router.navigate(['servers'], { relativeTo: this.route });
+  }
 }
