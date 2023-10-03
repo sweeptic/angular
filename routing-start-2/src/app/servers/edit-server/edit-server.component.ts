59438535f05c732574918c1c6620808da99cac00
Attila Horvath <sweeptic@gmail.com>
2023-10-03 11:40:09 +0200

    145 - Outsourcing the Route Configuration

--- routing-start-2/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start-2/src/app/servers/edit-server/edit-server.component.ts
@@ -22,7 +22,7 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
     this.route.queryParams.subscribe((queryParams: Params) => {
-      this.allowEdit = queryParams['allowEdit'] === 1 ? true : false;
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
     });
     this.route.fragment.subscribe();
 
