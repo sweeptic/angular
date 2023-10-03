a480ec2a8999be76b5a45d3db4b3b596e9b9f296
Attila Horvath <sweeptic@gmail.com>
2023-10-03 12:10:42 +0200

    149 - Using a Fake Auth Service

---
+++ routing-start-2/src/app/auth-guard.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+    this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): any {
+    return this.canActivate(route, state);
+  }
+}
