0f8df11b954ec45510360f832c33d5b3c68d5d1b
Attila Horvath <sweeptic@gmail.com>
2023-10-04 18:04:24 +0200

    add router link

--- recipe_book/src/app/main-elements/header/header-component.ts
+++ recipe_book/src/app/main-elements/header/header-component.ts
@@ -5,11 +5,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './header-component.html',
 })
 export class HeaderComponent {
-  @Output() selectedMenu = new EventEmitter<string>();
-
-  selectMenu(page: string) {
-    this.selectedMenu.emit(page);
-  }
-
   constructor() {}
 }
