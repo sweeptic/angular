2b55e9d8a31be08e44b0737ab0aeb46dc7246ab2
Attila Horvath <sweeptic@gmail.com>
2023-10-05 14:05:40 +0200

    173 - Getting Closer to the Core of Observables

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private firstObsSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
