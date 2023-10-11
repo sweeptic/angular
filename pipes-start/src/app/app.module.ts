cf0865d5caa62850002b77b22be32b7ec6e31dca
Attila Horvath <sweeptic@gmail.com>
2023-10-11 11:33:06 +0200

    250 - Understanding the async Pipe

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -2,18 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
+import { ShortenPipe } from 'src/shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+// type ErrorCode = string | number;
+// interface Answer {
+//     string | number;
+// }
+
+// type User = {
+//   id: number;
+//   name: string;
+//   email: string;
+// };
