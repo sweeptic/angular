aa1a429cbb2409c3d0831c9f031811d4fad38576
Attila Horvath <sweeptic@gmail.com>
2023-10-11 16:24:34 +0200

    276 - Multiple Interceptors

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -13,16 +13,17 @@ export class AuthInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log('re is on its way');
+    // console.log('re is on its way');
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
-    return next.handle(modifiedRequest).pipe(
-      tap((event) => {
-        if (event.type === HttpEventType.Response) {
-          console.log('response arrived: ', event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
+    // return next.handle(modifiedRequest).pipe(
+    //   tap((event) => {
+    //     if (event.type === HttpEventType.Response) {
+    //       console.log('response arrived: ', event.body);
+    //     }
+    //   })
+    // );
   }
 }
