776aef9e5cc1f5b2368b60c424512c2a32f750a1
Attila Horvath <sweeptic@gmail.com>
2023-10-16 12:03:17 +0200

    313 - Using ngIf

--- recipe_book/src/app/shared/alert/alert-component.ts
+++ recipe_book/src/app/shared/alert/alert-component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -7,8 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AlertComponent {
   @Input() message: string;
+  @Output() close = new EventEmitter<void>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  onClose() {
+    this.close.emit();
+  }
 }
