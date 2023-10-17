29658ccc43aa6a87c2fc58670efc5060d5cc4462
Attila Horvath <sweeptic@gmail.com>
2023-10-17 09:15:34 +0200

    334 - More Lazy Loading

--- recipe_book/src/app/app.module.ts
+++ recipe_book/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { HeaderComponent } from './header/header-component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -17,10 +15,8 @@ import { AuthModule } from './auth/auth.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
