f7b57254f19799f50939f8f381694e97b3f61279
Attila Horvath <sweeptic@gmail.com>
2023-10-17 12:32:31 +0200

    352 - Services Standalone Components

--- standalone/src/main.ts
+++ standalone/src/main.ts
@@ -2,9 +2,15 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(
+  AppComponent
+  //     {
+  //   providers: [AnalyticsService],
+  //     }
+);
