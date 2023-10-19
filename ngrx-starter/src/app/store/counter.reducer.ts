8ab961c78c6c5f822ee9d18e638c081b05639d01
Attila Horvath <sweeptic@gmail.com>
2023-10-19 13:45:04 +0200

    376 - Handling Actions Without createReducer

--- ngrx-starter/src/app/store/counter.reducer.ts
+++ ngrx-starter/src/app/store/counter.reducer.ts
@@ -1,13 +1,24 @@
 import { createReducer, on } from '@ngrx/store';
-import { increment } from './counter.actions';
+// import { increment } from './counter.actions';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
-// export function counterReducer(state = initialState) {
-//   return state;
-// }
+export function counterReducer(state = initialState, action: any) {
+  let newState = state;
+
+  switch (action.type) {
+    case '[Counter] Increment':
+      newState = state + action.value;
+      break;
+
+    default:
+      break;
+  }
+
+  return newState;
+}
