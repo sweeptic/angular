f9fba06d10d5d7c456f679832b84652d0f61ec16
Attila Horvath <sweeptic@gmail.com>
2023-10-19 13:37:07 +0200

    375 - Attaching Data To Actions

--- ngrx-starter/src/app/counter-controls/counter-controls.component.ts
+++ ngrx-starter/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +9,11 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment({ value: 1 }));
   }
 
-  decrement() {
-    this.counterService.decrement();
-  }
+  decrement() {}
 }
--- ngrx-starter/src/app/store/counter.actions.ts
+++ ngrx-starter/src/app/store/counter.actions.ts
@@ -1,3 +1,6 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
-export const increment = createAction('[Counter] Increment');
+export const increment = createAction(
+  '[Counter] Increment',
+  props<{ value: number }>()
+);
