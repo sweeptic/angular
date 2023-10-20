7de47a96a6f84d7fa494c25b47ffb2cf12084104
Attila Horvath <sweeptic@gmail.com>
2023-10-20 10:36:09 +0200

    385 - Adding a Second Effect

--- ngrx-starter/src/app/store/counter.effects.ts
+++ ngrx-starter/src/app/store/counter.effects.ts
@@ -1,12 +1,25 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { decrement, increment } from './counter.actions';
-import { tap, withLatestFrom } from 'rxjs';
+import { decrement, increment, init, set } from './counter.actions';
+import { of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectCount } from './counter.selectors';
 
 @Injectable()
 export class CounterEffects {
+  loadCount = createEffect(() =>
+    this.actions$.pipe(
+      ofType(init),
+      switchMap(() => {
+        const storedCounter = localStorage.getItem('count');
+        if (storedCounter) {
+          return of(set({ value: +storedCounter }));
+        }
+        return of(set({ value: 0 }));
+      })
+    )
+  );
+
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
