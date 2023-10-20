d4263bf008789f6d486f33f8ec6d6e036dd7e9c4
Attila Horvath <sweeptic@gmail.com>
2023-10-20 09:47:28 +0200

    382 - Defining a First Effect

--- ngrx-starter/src/main.ts
+++ ngrx-starter/src/main.ts
@@ -3,7 +3,8 @@ import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideStore({ counter: counterReducer })],
+  providers: [provideStore({ counter: counterReducer }), provideEffects()],
 });
