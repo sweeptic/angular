3e96b283ed0fa6b32ba04e23378d7267cb6f1d85
Attila Horvath <sweeptic@gmail.com>
2023-10-20 12:43:18 +0200

    396 - Preparing Update Delete Actions

--- recipe_book/src/app/shopping-list/store/shopping-list.action.ts
+++ recipe_book/src/app/shopping-list/store/shopping-list.action.ts
@@ -3,18 +3,31 @@ import { Ingredient } from 'src/app/shared/ingredient-model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
-  // payload: Ingredient;
-
   constructor(public payload: Ingredient) {}
 }
 
 export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
-
   constructor(public payload: Ingredient[]) {}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENTS;
+  constructor(public payload: { index: number; ingredient: Ingredient }) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENTS;
+  constructor(public payload: number) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredient
+  | AddIngredients
+  | UpdateIngredient
+  | DeleteIngredient;
