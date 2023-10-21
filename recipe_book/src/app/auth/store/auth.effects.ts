b47a903036b58e68dc9b3d163d70d134ffc205d5
Attila Horvath <sweeptic@gmail.com>
2023-10-21 10:19:02 +0200

    408 - Defining the First Effect

---
+++ recipe_book/src/app/auth/store/auth.effects.ts
@@ -0,0 +1,8 @@
+import { Actions, ofType } from '@ngrx/effects';
+import { LOGIN_START } from './auth.actions';
+
+export class AuthEffects {
+  authLogin = this.actions$.pipe(ofType(LOGIN_START));
+
+  constructor(private actions$: Actions) {}
+}
